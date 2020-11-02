b46e1ce0c6a486b80d15161ea6ee4f36b7051a34
nickverheyen8 <nickverheyen8@gmail.com>
2020-11-02 09:18:45 -0500

    dice roll working

--- Dice Roll 2/ViewController.swift
+++ Dice Roll 2/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    let diceImages = ["die1","die2","die3","die4","die5","die6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView1.image = UIImage(named: "die6")
+        imageView2.image = UIImage(named: "die3")
+        
     }
 
-
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    
+    @IBAction func rollDice(_ sender: UIButton) {
+    
+        imageView1.image = UIImage(named: diceImages[Int(arc4random_uniform(6))])
+    imageView2.image = UIImage(named: diceImages[Int(arc4random_uniform(6))])
+    }
+    
 }
 
